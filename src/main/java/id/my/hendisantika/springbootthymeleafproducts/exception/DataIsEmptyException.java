064f41b514dae4254936265744299450674a8ce4
hendisantika <hendisantika@yahoo.co.id>
2024-02-25 11:23:24 +0700

    Add DataIsEmptyException🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootthymeleafproducts/exception/DataIsEmptyException.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.springbootthymeleafproducts.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-thymeleaf-products
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/25/24
+ * Time: 11:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataIsEmptyException extends RuntimeException {
+}
