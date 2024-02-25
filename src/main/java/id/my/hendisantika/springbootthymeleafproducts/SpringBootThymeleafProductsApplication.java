a5f3eab73accbde1520b16983dc7c6acb90d405e
hendisantika <hendisantika@yahoo.co.id>
2024-02-25 11:14:44 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootthymeleafproducts/SpringBootThymeleafProductsApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootthymeleafproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThymeleafProductsApplication.class, args);
+    }
+
+}
