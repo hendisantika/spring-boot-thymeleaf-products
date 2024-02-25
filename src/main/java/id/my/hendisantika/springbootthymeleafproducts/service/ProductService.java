3cb5a471b96a90a3ddc0a7f065f855d7190991c2
hendisantika <hendisantika@yahoo.co.id>
2024-02-25 11:26:18 +0700

    Add ProductService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootthymeleafproducts/service/ProductService.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.springbootthymeleafproducts.service;
+
+import id.my.hendisantika.springbootthymeleafproducts.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-thymeleaf-products
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/25/24
+ * Time: 11:25
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+}
